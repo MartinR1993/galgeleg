d8fc1563c2c723ae28d0c1cec3ccbe75eb6e3786
Asger Stage <asger.stage@hotmail.com>
2017-05-01 21:17:04 +0200

    JsonUtil behandler nu også Boolean

--- src/utils/JsonUtil.java
+++ src/utils/JsonUtil.java
@@ -5,23 +5,39 @@ import com.google.gson.Gson;
 import spark.ResponseTransformer;
 
 public class JsonUtil {
-	 
-	  public static String toJson(Object object) {	
-		  if(object instanceof String){
-			 NestedUser navn=new NestedUser((String)object);
-			 return new Gson().toJson(navn);
-		  }
-	    return new Gson().toJson(object);
-	  }
-	 
-	  public static ResponseTransformer json() {
-	    return JsonUtil::toJson;
-	  }
-	  
+
+	public static String toJson(Object object) {
+		
+		//Checker om input er en streng eller boolean, og hvis det er bliver det pakket ind i en klasse
+		//så det kan konverteres til Json
+		if (object instanceof String) {
+			NestedStringConverter key = new NestedStringConverter((String) object);
+			return new Gson().toJson(key);
+		} else if (object instanceof Boolean) {
+			NestedBooleanConverter key = new NestedBooleanConverter((Boolean) object);
+			return new Gson().toJson(key);
+		}
+		return new Gson().toJson(object);
 	}
-class NestedUser {
-    String key;
-    NestedUser(String key){
-    	this.key=key;
-    }
+
+	public static ResponseTransformer json() {
+		return JsonUtil::toJson;
+	}
+
+}
+
+class NestedStringConverter {
+	String key;
+
+	NestedStringConverter(String key) {
+		this.key = key;
+	}
+}
+
+class NestedBooleanConverter {
+	Boolean key;
+
+	NestedBooleanConverter(Boolean key) {
+		this.key = key;
+	}
 }
