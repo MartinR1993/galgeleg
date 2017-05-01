619803a0aae515550de0db90db2340d03962fe26
Asger Stage <asger.stage@hotmail.com>
2017-05-01 12:54:01 +0200

    Opdateret REST

--- src/utils/JsonUtil.java
+++ src/utils/JsonUtil.java
@@ -6,11 +6,22 @@ import spark.ResponseTransformer;
 
 public class JsonUtil {
 	 
-	  public static String toJson(Object object) {
+	  public static String toJson(Object object) {	
+		  if(object instanceof String){
+			 NestedUser navn=new NestedUser((String)object);
+			 return new Gson().toJson(navn);
+		  }
 	    return new Gson().toJson(object);
 	  }
 	 
 	  public static ResponseTransformer json() {
 	    return JsonUtil::toJson;
 	  }
+	  
 	}
+class NestedUser {
+    String key;
+    NestedUser(String key){
+    	this.key=key;
+    }
+}
