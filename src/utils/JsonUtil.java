b4e116db4223b9e747b85b64c67c5705e931dd9e
Asger Stage <asger.stage@hotmail.com>
2017-05-02 17:47:56 +0200

    JsonUtil opdateret til at tage imod ArrayList
    Tilføjet relativ path til mysqlconnector

--- src/utils/JsonUtil.java
+++ src/utils/JsonUtil.java
@@ -1,5 +1,7 @@
 package utils;
 
+import java.util.ArrayList;
+
 import com.google.gson.Gson;
 
 import spark.ResponseTransformer;
@@ -7,14 +9,18 @@ import spark.ResponseTransformer;
 public class JsonUtil {
 
 	public static String toJson(Object object) {
-		
-		//Checker om input er en streng eller boolean, og hvis det er bliver det pakket ind i en klasse
-		//så det kan konverteres til Json
+
+		// Checker om input er en streng eller boolean, og hvis det er bliver
+		// det pakket ind i en klasse
+		// så det kan konverteres til Json
 		if (object instanceof String) {
 			NestedStringConverter key = new NestedStringConverter((String) object);
 			return new Gson().toJson(key);
 		} else if (object instanceof Boolean) {
 			NestedBooleanConverter key = new NestedBooleanConverter((Boolean) object);
+			return new Gson().toJson(key);
+		} else if (object instanceof ArrayList<?>) {
+			NestedListConverter key = new NestedListConverter((ArrayList<String>) object);
 			return new Gson().toJson(key);
 		}
 		return new Gson().toJson(object);
@@ -38,6 +44,14 @@ class NestedBooleanConverter {
 	Boolean key;
 
 	NestedBooleanConverter(Boolean key) {
+		this.key = key;
+	}
+}
+
+class NestedListConverter {
+	ArrayList<String> key;
+
+	NestedListConverter(ArrayList<String> key) {
 		this.key = key;
 	}
 }
